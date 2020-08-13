4ed9fc92bd73d1feba0a81122c0fad54267b5746
Michail Bidnyk <mbidnyk1@gmail.com>
2020-08-12 22:17:17 -0500

    test: ds-router endpoint

--- ds/ds-router.js
+++ ds/ds-router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 Dsmodel = require('./ds-model')
 
-router.get('/viz/:state', async (req, res,next) => {
+router.get('/viz', async (req, res,next) => {
     try{
-        const stateStr = String(req.params.state);
-        const stateObj = {state: stateStr}
-        await dsModel.add(stateObj)
+        // const stateStr = String(req.params.state);
+        // const stateObj = {state: stateStr}
+        // await dsModel.add(stateObj)
         const data = await dsModel.getData()
         res.status(200).json(data)
       } catch(err) {
