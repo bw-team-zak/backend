eaf8991df05d4eeb4d34df1c380e9d018bc18e84
Michail Bidnyk <mbidnyk1@gmail.com>
2020-08-12 21:17:17 -0500

    fix: ds-data.js seed reformat

--- data/migrations/20200812135613_ds-data.js
+++ data/migrations/20200812135613_ds-data.js
@@ -2,7 +2,7 @@ exports.up = async function(knex) {
     await knex.schema.createTable('ds_data', data => {
         data.increments();
     
-        data.specificType('data','jsonb');
+        data.specificType('data','jsonb[]');
         data.specificType('layout','jsonb')
         data.string('state')
     }) 
