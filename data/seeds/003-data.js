d179027087c42cf9e9aca4fadba874b51c82862b
Michail Bidnyk <mbidnyk1@gmail.com>
2020-08-12 21:04:01 -0500

    Fix: edited seed data to correct format

--- data/seeds/003-data.js
+++ data/seeds/003-data.js
@@ -1,18 +1,19 @@
 exports.seed = function (knex) {
-    const DSdata = {
-        data:
-        [
-            {
-            type: 'pie',
-            values: [19, 26, 55],
-            labels: ['Food', 'Rent', 'Utility'],
-            }
-        ],
-        layout:
-        { width: 420, height: 540, title: 'Your Savings' },
-       
-        state: '07-09'
-    }
-
+    const DSdata = [
+        {
+            data:
+            [
+                {
+                type: 'pie',
+                values: [19, 26, 55],
+                labels: ['Food', 'Rent', 'Utility'],
+                }
+            ],
+            layout:
+                { width: 420, height: 540, title: 'Your Savings' },
+        
+            state: '07-09'
+        }
+    ]
     return knex('ds_data').insert(DSdata);
 };
