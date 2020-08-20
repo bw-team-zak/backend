1eff06e397e2ccd4c099ead7337a87283958e99c
Michail Bidnyk <mbidnyk1@gmail.com>
2020-08-19 19:52:03 -0500

    update spending endpoint

--- ds/ds-router.js
+++ ds/ds-router.js
@@ -5,7 +5,7 @@ router.post('/spending', async (req, res,next) => {
     try{
         const user_ID = req.body.user_ID
         const data = await Dsmodel.getData(user_ID)
-        res.status(200).json(data)
+        res.status(200).json(data[0].graph_data)
       } catch(err) {
           next(err)
       }
